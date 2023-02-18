47684dacf7e4d6e76a34d6950605854214e96e61
Sergio Garzón <sergio.garzon.mar@gmail.com>
2023-02-17 21:50:31 -0500

    Changes in the url base_dir variable at the settings.py file

--- PapAppCost/PapAppCost/settings.py
+++ PapAppCost/PapAppCost/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = 'https://papapp-cost-umb.vercel.app/'
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
