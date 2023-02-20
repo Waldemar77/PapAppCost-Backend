4d1eaf17bfdfbf7944208ccce9e166789e0fb014
Sergio Garzón <sergio.garzon.mar@gmail.com>
2023-02-20 15:07:15 -0500

    Changes in settings.py file, trying to set the driver SQL odbc

--- PapAppCost/PapAppCost/settings.py
+++ PapAppCost/PapAppCost/settings.py
@@ -108,8 +108,8 @@ WSGI_APPLICATION = 'PapAppCost.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'mssqlms://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:1433/{os.getenv("DB_NAME")}',
-        conn_max_age=600
+        default=f'mssqlms://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:1433/{os.getenv("DB_NAME")}?driver=ODBC Driver 18 for SQL Server',
+        conn_max_age=600,
     )    
     # 'default': {
     #     'ENGINE': 'mssql',
