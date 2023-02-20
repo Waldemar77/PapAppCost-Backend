3a38cc31dabaae07cd0855e9878597d7b7d31aa7
Sergio Garzón <sergio.garzon.mar@gmail.com>
2023-02-20 11:11:32 -0500

    Ready for deploy on Render.com editing os.getenv methods

--- PapAppCost/PapAppCost/settings.py
+++ PapAppCost/PapAppCost/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='your secret key')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -34,7 +34,7 @@ DEBUG = 'RENDER' not in os.environ
 # set settings for Render host site
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
